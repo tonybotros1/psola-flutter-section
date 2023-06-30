c9ca2b212b85e9ec8948179aee6cdadbffaace2f
GOWaz <abdullahalzabadani@gmail.com>
2023-06-30 15:33:51 +0300

    recorder

--- lib/screen/MainPage.dart
+++ lib/screen/MainPage.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:psola/constants.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -9,6 +12,43 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final recorder = FlutterSoundRecorder();
+  bool isRecorderReady = false;
+
+  @override
+  void initState() {
+    initRecorder();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    recorder.closeRecorder();
+    super.dispose();
+  }
+
+  Future initRecorder() async {
+    final state = await Permission.microphone.request();
+    if (state != PermissionStatus.granted) {
+      throw 'Microphone permission not granted';
+    }
+    await recorder.openRecorder();
+    isRecorderReady = true;
+    recorder.setSubscriptionDuration(const Duration(milliseconds: 500));
+  }
+
+  Future record() async {
+    if (!isRecorderReady) return;
+    await recorder.startRecorder(toFile: 'audio');
+
+  }
+  Future stop() async {
+    if (!isRecorderReady) return;
+    final path = await recorder.stopRecorder();
+    final audioFile = File(path!);
+    print(audioFile);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +57,35 @@ class _MainPageState extends State<MainPage> {
         backgroundColor: backgroundColor,
       ),
       backgroundColor: backgroundColor,
-      body: const Column(
-        children: [
-          Text("data")
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamBuilder<RecordingDisposition>(
+                stream: recorder.onProgress,
+                builder: (context, snapshot) {
+                  final duration = snapshot.hasData
+                      ? snapshot.data!.duration
+                      : Duration.zero;
+                  String twoDigits(int n) => n.toString().padLeft(2,'0');
+                  final twoDigitMinutes =
+                      twoDigits(duration.inMinutes.remainder(60));
+                  final twoDigitSeconds =
+                      twoDigits(duration.inSeconds.remainder(60));
+                  return Text('$twoDigitMinutes:$twoDigitSeconds');
+                }),
+            ElevatedButton(
+                onPressed: () async {
+                  if (recorder.isRecording) {
+                    await stop();
+                  } else {
+                    await record();
+                  }
+                  setState(() {});
+                },
+                child: Icon(recorder.isRecording ? Icons.stop : Icons.mic)),
+          ],
+        ),
       ),
     );
   }
